fe77e585748e5537686dce8311c0773451ee4f1a
Alex <skyriver.f1@gmail.com>
2020-12-16 18:31:50 +0500

    slider shares, mini cart lots

--- static/js/app.js
+++ static/js/app.js
@@ -308,4 +308,23 @@ $(function () {
       }
     });
   }
+
+  if ($.exists('.js-shares-slider')) {
+    new Splide('.js-shares-slider', {
+      perPage: 4,
+      rewind: true,
+      pagination: false,
+      breakpoints: {
+        1279: {
+          perPage: 3
+        },
+        959: {
+          perPage: 2
+        },
+        767: {
+          perPage: 1
+        }
+      }
+    }).mount();
+  }
 });
